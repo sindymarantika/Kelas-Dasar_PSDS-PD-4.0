826caff481ae657fec46ea36b003c490c4c89ce8
Sindy Mei Marantika <121431792+sindymarantika@users.noreply.github.com>
2023-06-17 14:35:20 +0700

    Delete Gagal teknologi.sql

--- SQL/Gagal teknologi.sql
+++
@@ -1,6 +0,0 @@
-select * from Penyebab_Kematian_di_Indonesia_yang_Dilaporkan
-
-select * from Penyebab_Kematian_di_Indonesia_yang_Dilaporkan where Cause in ('Gagal Teknologi',
-																			'Kegagalan Teknologi',
-																			'Kegagalan Teknologi (termasuk ledakan kompor gas)')
-
