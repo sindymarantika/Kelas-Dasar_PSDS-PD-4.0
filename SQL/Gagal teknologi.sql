3b929266ad3ce461c01ba74ff73943470163c98f
Sindy Mei Marantika <121431792+sindymarantika@users.noreply.github.com>
2023-06-17 14:33:58 +0700

    Add files via upload

---
+++ SQL/Gagal teknologi.sql
@@ -0,0 +1,6 @@
+select * from Penyebab_Kematian_di_Indonesia_yang_Dilaporkan
+
+select * from Penyebab_Kematian_di_Indonesia_yang_Dilaporkan where Cause in ('Gagal Teknologi',
+																			'Kegagalan Teknologi',
+																			'Kegagalan Teknologi (termasuk ledakan kompor gas)')
+
